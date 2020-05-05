2b99fd06c1543df584a6fbc6d9fe77d40322b559
pushkar.goel <pushkar.goel@paytm.com>
2020-05-06 00:38:07 +0530

    added site maps

--- pages/director/[din].tsx
+++ pages/director/[din].tsx
@@ -36,61 +36,64 @@ const Home: NextPage<IDirectorPage.IProps, IDirectorPage.InitialProps> = ({
     const home = useSelector((state: IStore) => state.home);
     const dispatch = useDispatch();
 
-    // remove later
-    const details = JSON.parse(dinData.data);
     const refData = [
         {
             title: "DIN",
-            value: details["DIN"]
+            value: dinData["DIN"]
         },
         {
             title: "Name",
-            value: details["nameOfApplicant"]
+            value: dinData["name"]
         },
         {
             title: "Date of birth",
-            value: details["dateOfBirth"]
+            value: dinData["dob"]
         },
         {
             title: "Father Name",
-            value: details["fatherName"]
+            value: '*********'
         },
         {
             title: "Mobile",
-            value: details["addMobile"]
+            value: '*********'
         },
         {
             title: "Phone",
-            value: details["addPhone"]
+            value: '*********'
         },
         {
             title: "Email",
-            value: details["addEmail"]
+            value: '*********'
         },
         {
             title: "Address",
-            value: details["permenantAddress"]
+            value: '*********'
         },
         {
             title: "City",
-            value: details["addCity"]
+            value: dinData["city"]
         },
         {
             title: "Gender",
-            value: details["flag5"]
+            value: dinData["gender"]
         }
     ];
 
     // remove later
-    details.companies = [
-        ["U74140DL2005PLC134597", "ACCORD FINANCIAL CONSULTANCY LIMITED", "29/11/2007", "-", "ACTIVE compliant"],
-        ["U74140DL2005PLC134597", "ACCORD FINANCIAL CONSULTANCY LIMITED", "29/11/2007", "-", "ACTIVE compliant"],
-        ["U74140DL2005PLC134597", "ACCORD FINANCIAL CONSULTANCY LIMITED", "29/11/2007", "-", "ACTIVE compliant"]
-    ];
 
-    details.llps = [
-        ["AAG-4536", "ACCORD FINANCIAL SERVICES LLP", "25/05/2016", "-"]
-    ];
+    if(!dinData.companies) {
+        dinData.companies = [
+            ["U74140DL2005PLC134597", "ACCORD FINANCIAL CONSULTANCY LIMITED", "29/11/2007", "-", "ACTIVE compliant"],
+            ["U74140DL2005PLC134597", "ACCORD FINANCIAL CONSULTANCY LIMITED", "29/11/2007", "-", "ACTIVE compliant"],
+            ["U74140DL2005PLC134597", "ACCORD FINANCIAL CONSULTANCY LIMITED", "29/11/2007", "-", "ACTIVE compliant"]
+        ];
+    }
+
+    if(!dinData.llps) {
+        dinData.llps = [
+            ["AAG-4536", "ACCORD FINANCIAL SERVICES LLP", "25/05/2016", "-"]
+        ];
+    }
 
     const renderLocaleButtons = (activeLanguage: string) =>
         ["en", "es", "tr"].map(lang => (
@@ -106,8 +109,8 @@ const Home: NextPage<IDirectorPage.IProps, IDirectorPage.InitialProps> = ({
         <Layout>
 
             <h1 className="my-4">
-                <small>{details["DIN"]}</small>
-                <div>{details["nameOfApplicant"]}</div>
+                <small>{dinData["DIN"]}</small>
+                <div>{dinData["name"]}</div>
             </h1>
             <div className="card mb-4">
                 <div className="card-body">
@@ -143,7 +146,7 @@ const Home: NextPage<IDirectorPage.IProps, IDirectorPage.InitialProps> = ({
                         </thead>
                         <tbody>
                             {
-                                details.companies.map((company, index) => {
+                                dinData.companies.map((company, index) => {
                                     return (
                                         <tr key={`director_${company[0]}`} className="text-semibold text-reagent-gray">
                                             <td>{index + 1}</td>
@@ -179,7 +182,7 @@ const Home: NextPage<IDirectorPage.IProps, IDirectorPage.InitialProps> = ({
                         </thead>
                         <tbody>
                             {
-                                details.llps.map((llp, index) => {
+                                dinData.llps.map((llp, index) => {
                                     return (
                                         <tr key={`charge_${llp[1]}`} className="text-semibold text-reagent-gray">
                                             <td>{index + 1}</td>
@@ -208,10 +211,7 @@ Home.getInitialProps = async (
     ctx: ReduxNextPageContext
 ): Promise<IDirectorPage.InitialProps> => {
     const { din } = ctx.query;
-    // const dinData = await Http.Request('GET', `http://3.7.5.125:4000/v1/director/${din}`);
-    const dinData = {
-        data: JSON.stringify({ "flag": ["0"], "flag1": ["NO"], "flag2": [""], "flag3": [""], "flag4": [""], "flag5": ["Female"], "flag6": [""], "flag7": ["IN"], "flag8": [""], "flag9": [""], "state": [""], "addFax": [""], "margin": [""], "tcShip": [""], "addCity": ["NEW DELHI"], "country": [""], "dateOne": ["1800-01-01T00:00:00+05:30"], "dateSix": ["1800-01-01T00:00:00+05:30"], "dateTwo": ["1800-01-01T00:00:00+05:30"], "llpName": [""], "paddFax": [""], "tcOther": [""], "addEmail": ["tulika_agarwal@rediffmail.com"], "addPhone": ["98711400817"], "addState": ["DL"], "dateFive": ["1800-01-01T00:00:00+05:30"], "dateFour": ["1800-01-01T00:00:00+05:30"], "nameLast": ["AGARWAL"], "paddCity": ["NEW DELHI"], "statName": [""], "tcPatent": [""], "totalOne": [""], "totalTwo": [""], "addMobile": ["9811400817"], "cinStatus": [""], "dateThree": ["1800-01-01T00:00:00+05:30"], "nameFirst": ["TULIKA"], "nameOfLLP": [""], "paddEmail": [""], "paddLine1": ["A-60, GROUND FLOOR"], "paddLine2": [""], "paddPhone": [""], "paddState": ["DL"], "profitOne": [""], "profitTwo": [""], "regNumber": [""], "srnStatus": [""], "tcCOthers": [""], "totalFour": [""], "addCNTRYNM": [""], "addCountry": ["IN"], "addLineOne": ["A-60, GROUND FLOOR"], "addLineTwo": [""], "addPinCode": ["110024"], "categoryOP": [""], "cinCompany": [""], "fatherName": ["NARENDRA"], "isoCountry": [""], "llpAddress": [""], "occupation": [""], "paddMobile": [""], "propBusAct": [""], "regOFCName": [""], "tcGoodWill": [""], "totalThree": [""], "addDistrict": [""], "companyName": [""], "dateOfBirth": ["1975-07-10T00:00:00+05:30"], "designation": [""], "dpinStatusN": [""], "nationality": ["IN"], "otherPRTCLR": [""], "paddCntryNM": [""], "paddCountry": ["IN"], "paddPinCode": ["110024"], "presentAddr": ["A-60, GROUND FLOOR$LAJPAT NAGAR-II$NEW DELHI$Delhi$110024$India$"], "profitThree": [""], "satisfyDate": ["1800-01-01T00:00:00+05:30"], "tcBookdbets": [""], "tcCopyRight": [""], "amountCharge": ["0.0"], "categoryothr": [""], "chargeModArc": [""], "dynStringOne": ["LAJPAT NAGAR-II"], "dynStringTwo": ["LAJPAT NAGAR-II"], "extNTOPRCHRG": [""], "nameRegister": [""], "paddDistrict": [""], "propNTREGLLP": [""], "tcIntimmProp": [""], "tcTradeMarks": [""], "conSORTIUMFIN": [""], "dateINSCHRGCR": ["1800-01-01T00:00:00+05:30"], "dateINSCHRGMD": ["1800-01-01T00:00:00+05:30"], "dateINSCRTEVI": ["1800-01-01T00:00:00+05:30"], "dynStringFive": [""], "dynStringFour": ["AGARWAL"], "dynStringThre": ["KISHORE"], "firstLastName": ["NARENDRA KISHORE AGARWAL"], "nameOfPartner": [""], "rateOfIntrest": [""], "tcImmProperty": [""], "tcmovableProp": [""], "addCertificate": [""], "chargeOutSDIND": [""], "companyAddress": [""], "dateRECEIPTIND": ["1800-01-01T00:00:00+05:30"], "descriptionOne": [""], "descriptionTwo": [""], "instrumentDESC": [""], "jointChargeINV": [""], "noChargeHolder": [""], "presentAddress": [""], "prtCLRPROPCHRG": [""], "prtCLRPRSNTMOD": [""], "shortPRTPROPCH": [""], "termsRePayment": [""], "dateAcquisition": ["1800-01-01T00:00:00+05:30"], "nameOfApplicant": ["TULIKA AGARWAL"], "residentOfIndia": ["YES"], "chargeHolderAuth": [""], "nameChargeHolder": [""], "permenantAddress": ["A-60, GROUND FLOOR$LAJPAT NAGAR-II$NEW DELHI$Delhi$110024$India$"], "tcFloatingCharge": [""], "instrumentDESCTwo": [""], "changedDesignation": [""], "detailChargeFRGNCR": [""] })
-    };
+    const dinData = await Http.Request('GET', `http://3.7.5.125:4000/v1/director/${din}`);
 
     await ctx.store.dispatch(
         HomeActions.GetApod({
